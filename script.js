47d4fa30970d3c9c907db503c6b3e607513e5edd
ADEIKA, Blessing Isoyiza <blessadeik@gmail.com>
2021-06-15 16:26:19 +0100

    gradient background

---
+++ script.js
@@ -0,0 +1,22 @@
+// Script for background generator
+
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+
+console.log(css);
+console.log(color1);
+console.log(color2);
+
+color1.addEventListener("input", function(){
+    body.style.background= 
+    "linear-gradient(to right," + color1.value + "," + color2.value + ")";
+
+})
+
+color2.addEventListener("input", function(){
+    body.style.background= 
+    "linear-gradient(to right," + color1.value + "," + color2.value + ")";
+})
